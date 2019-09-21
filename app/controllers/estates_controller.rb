2db33ef7115ab0520f4c1fed7fa063bafef7525c
broof716 <broof716@gmail.com>
2019-09-21 06:03:27 +0000

    adding devise gem

--- app/controllers/estates_controller.rb
+++ app/controllers/estates_controller.rb
@@ -8,4 +8,15 @@ class EstatesController < ApplicationController
   @estate = Estate.new
  end
 
+  def create
+    Estate.create(estate_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def estate_params
+    params.require(:estate).permit(:name, :address)
+  end
+
 end
