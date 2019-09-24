a1c59827974cc112349bf67a48800507729ea7e3
broof716 <broof716@gmail.com>
2019-09-24 02:35:05 +0000

    adding comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+ before_action :authenticate_user!
+
+  def create
+    @estate = Estate.find(params[:estate_id])
+    @estate.comments.create(comments_params.merge(user: current_user))
+    redirect_to estate_path(@estate)
+  end
+
+  private
+
+    def comment_params
+    params.require(:comment).permit(:message, :rating)
+  end
+end
